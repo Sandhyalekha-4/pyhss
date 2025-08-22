d6aa9e918be4569ae935b366587261dd3498fb77
Sandhyalekha-4 <sandhyamukkala4@gmail.com>
2025-08-22 17:55:49 +0530

    Update georedService.py

--- services/georedService.py
+++ services/georedService.py
@@ -412,10 +412,6 @@ class GeoredService:
                 if not len(self.georedPeers) > 0:
                     georedEnabled = False
             
-            if self.webhookPeers is not None:
-                if not len(self.webhookPeers) > 0:
-                    webhooksEnabled = False
-
             if not georedEnabled and not webhooksEnabled:
                 await(self.logTool.logAsync(service='Geored', level='info', message=f"[Geored] [startService] Geored and Webhook services both disabled or missing peers, exiting."))
                 sys.exit()
